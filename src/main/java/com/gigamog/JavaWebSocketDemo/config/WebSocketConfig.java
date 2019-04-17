cde94d7be646268601a8a640d711b68ad907d287
alejandrade <alejandrodanielandrade@gmail.com>
2019-04-17 19:33:48 -0400

    first commit

--- src/main/java/com/gigamog/JavaWebSocketDemo/config/WebSocketConfig.java
+++ src/main/java/com/gigamog/JavaWebSocketDemo/config/WebSocketConfig.java
@@ -33,7 +33,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
              builder
                      .port(port)
                      .host(host)
-                     .secure()
         , new StompReactorNettyCodec());
         config
                 .setApplicationDestinationPrefixes("/app")
